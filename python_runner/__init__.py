298962f9bb69346e7045c97180708146582e1584
Alex Hall <alex.mojaki@gmail.com>
2022-01-30 12:47:59 +0200

    Can't cover __version__ ImportError, it's for development

--- python_runner/__init__.py
+++ python_runner/__init__.py
@@ -2,6 +2,6 @@ from .runner import Runner, PatchedStdinRunner, PatchedSleepRunner
 
 try:
     from .version import __version__
-except ImportError:
+except ImportError:  # pragma: no cover
     # version.py is auto-generated with the git tag when building
     __version__ = "???"
