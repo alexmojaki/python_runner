4dc9f2c348bb29910c9b0a8c54dbd20715777134
Alex Hall <alex.mojaki@gmail.com>
2022-01-30 12:43:52 +0200

    __version__

--- python_runner/__init__.py
+++ python_runner/__init__.py
@@ -1,1 +1,7 @@
 from .runner import Runner, PatchedStdinRunner, PatchedSleepRunner
+
+try:
+    from .version import __version__
+except ImportError:
+    # version.py is auto-generated with the git tag when building
+    __version__ = "???"
